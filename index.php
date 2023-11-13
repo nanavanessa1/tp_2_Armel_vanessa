5be294fa1b9c1f3b447eec066b1b2c8d89fb763b
vanessa <armelkiyetenanavanessa@gmail.com>
2023-11-13 09:08:25 -0500

    ajout de css sur la page d'accuiel

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+    <title>Veuillez entrer le nombre d'adresse svp </title>
+    <style>
+        title{
+            color:black;
+        }
+        body{
+            background-image: url("luxury.jpg");
+            background-size: cover; /* Ajuste la taille de l'image pour remplir le conteneur */
+            background-position: center; /* Positionne l'image au centre */
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 92vh;
+            margin: 5;
+        }
+        .highlight{
+            background-color: yellow; 
+        }
+        
+        .groupe{
+            color:black;
+        }
+        .content{
+            color :green;
+            display: center;
+            
+        }
+        .group{
+            display:center;
+        }
+        .red-button {
+        background-color: red;
+        color: white;
+        }
+        label{
+            color: white;
+            display: center;
+            
+          
+        }
+        
+        
+        </style>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="groupe">
+    <h1 class ="highlight">BIENVENUE A PLAZA HOTEL</H1>
+
+<div class ="content">
+      <h2>Veuillez entrez le  nombre d'adresses</h2>
+        <form method="post" action="analyse.php">
+            <div >
+                <label for="nombre adresses">Combien d'adresses avez-vous ?</label></br>
+                <input type="number" id="nombre adresses" name="nombre adresses" min="1" required>
+            </div>
+            <div class="group">
+           
+            <input type="submit" value="Suivant" style="background-color: orange; color: white;">
+    </div>
+        </form>
+    </div>
+    
+</body>
+</html>
